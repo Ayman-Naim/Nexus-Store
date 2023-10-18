3020ef32a287a8d11d2c6675ca5ff7643e173c51
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-18 14:47:11 -0700

    Create CartProductCell

--- Nexus-Store/Scenes/Cart/View/CartViewController.swift
+++ Nexus-Store/Scenes/Cart/View/CartViewController.swift
@@ -16,6 +16,17 @@ class CartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "My Cart"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.register(CartProductTableViewCell.nib(), forCellReuseIdentifier: CartProductTableViewCell.identifier)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
     
@@ -23,4 +34,23 @@ class CartViewController: UIViewController {
         print("checkoutButtonPressed")
     }
     
+}
+
+
+
+// MARK: - UITableView DataSource
+extension CartViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CartProductTableViewCell.identifier, for: indexPath) as! CartProductTableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.frame.size.height * 0.25
+    }
 }
