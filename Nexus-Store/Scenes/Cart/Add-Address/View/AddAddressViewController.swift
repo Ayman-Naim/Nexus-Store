be928f98a4bd44718860be8f918f8fb6c0f95436
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-19 12:58:44 -0700

    Add Actions to elements

--- Nexus-Store/Scenes/Cart/Add-Address/View/AddAddressViewController.swift
+++ Nexus-Store/Scenes/Cart/Add-Address/View/AddAddressViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
+
+protocol AddAddressDelegate: AnyObject {
+    func didAddNewAddress()
+}
+
+
 class AddAddressViewController: UIViewController {
     
-    
     private let addAddressView = AddAddressView()
+    weak var delegate: AddAddressDelegate?
 
     override func loadView() {
         super.loadView()
@@ -22,8 +28,57 @@ class AddAddressViewController: UIViewController {
         title = "Add Address"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
+        
+        setupElements()
+        
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    private func setupElements() {
+        addAddressView.nameTextField.delegate = self
+        addAddressView.cityTextField.delegate = self
+        addAddressView.addressTextField.delegate = self
+        
+        addAddressView.addButton.addTarget(self, action: #selector(addButtonPressed), for: .touchUpInside)
     }
     
     
+    @objc private func addButtonPressed() {
+        guard
+            let name = addAddressView.nameTextField.text,
+            let city = addAddressView.cityTextField.text,
+            let address = addAddressView.addressTextField.text
+        else {
+            return
+        }
+        
+        print("\(name) | \(city) | \(address)")
+        
+        delegate?.didAddNewAddress()
+    }
     
+    deinit {
+        print("AddAddressViewController deinit")
+    }
+}
+
+
+// MARK: - UITextField Delegate
+extension AddAddressViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case addAddressView.nameTextField:
+            addAddressView.cityTextField.becomeFirstResponder()
+            return false
+            
+        case addAddressView.cityTextField:
+            addAddressView.addressTextField.becomeFirstResponder()
+            return false
+            
+        default:
+            textField.endEditing(true)
+            return true
+        }
+    }
 }
