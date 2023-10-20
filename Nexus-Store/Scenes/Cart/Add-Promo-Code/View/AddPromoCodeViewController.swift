5ab6e9d09d6bf7bdd4c073b755b1f9578811f1e3
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-20 13:02:43 -0700

    Create Add PromoCode Screen

--- Nexus-Store/Scenes/Cart/Add-Promo-Code/View/AddPromoCodeViewController.swift
+++ Nexus-Store/Scenes/Cart/Add-Promo-Code/View/AddPromoCodeViewController.swift
@@ -8,8 +8,37 @@
 import UIKit
 
 class AddPromoCodeViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var promoCodeTextField: CustomTextField!
+    @IBOutlet weak var promoCodeLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var promoDiscountLabel: UILabel!
+    @IBOutlet weak var totalPriceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Add Promo Code"
+        
+        promoCodeTextField.delegate = self
+    }
+    
+    @IBAction func plusButtonPressed(_ sender: UIButton) {
+        guard let promoCode = promoCodeTextField.text, !promoCode.isEmpty else {
+            return
+        }
+        
+        promoCodeLabel.text = "#" + promoCode
+    }
+    
+    @IBAction func showOrdersButtonPressed(_ sender: UIButton) {
+    }
+}
+
+
+// MARK: - UITextField Delegate
+extension AddPromoCodeViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
     }
 }
