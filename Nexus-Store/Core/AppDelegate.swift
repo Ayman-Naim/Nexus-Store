7139ffffd4be6e8c5069926d5174c880c98fd5f8
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-19 18:07:46 -0700

    Remove Firebase

--- Nexus-Store/Core/AppDelegate.swift
+++ Nexus-Store/Core/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import CoreData
-import FirebaseCore
+//import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
+//        FirebaseApp.configure()
         return true
     }
 
