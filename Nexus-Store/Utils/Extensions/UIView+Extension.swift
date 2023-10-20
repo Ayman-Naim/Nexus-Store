bb938d438bd442bc24eeda2c37cf439dee1b94e2
MustafaaAdell <Mustafaadell9777@gmail.com>
2023-10-20 18:27:25 +0300

    Enhance Product Details View With scroll View and adding action to size and color cell

---
+++ Nexus-Store/Utils/Extensions/UIView+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  UIView+Extension.swift
+//  Nexus-Store
+//
+//  Created by Mustafa on 20/10/2023.
+//
+
+import UIKit
+
+
+extension UIView{
+    func addingShadowWithEffectToView(){
+        let shadows = UIView()
+        shadows.frame = self.frame
+        shadows.clipsToBounds = false
+        self.addSubview(shadows)
+        let shadowPath0 = UIBezierPath(roundedRect: shadows.bounds, cornerRadius: 8)
+        let layer0 = CALayer()
+        layer0.shadowPath = shadowPath0.cgPath
+        layer0.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.05).cgColor
+        layer0.shadowOpacity = 1
+        layer0.shadowRadius = 25
+        layer0.shadowOffset = CGSize(width: 0, height: 1)
+        layer0.bounds = shadows.bounds
+        layer0.position = shadows.center
+        shadows.layer.addSublayer(layer0)
+
+    }
+}
