e40c1d5573373ef9e903723848f33f077cd371ea
AhmedAshraf <ahmed.ashraf.AA.AS@gmail.com>
2023-10-27 13:57:11 +0200

    Create Account Firebase

--- Nexus-Store/Scenes/SignUp/View/SignUpViewController.swift
+++ Nexus-Store/Scenes/SignUp/View/SignUpViewController.swift
@@ -20,7 +20,30 @@ class SignUpViewController: UIViewController {
     }
 
     @IBAction func signUpButton(_ sender: Any) {
+        guard let email = emailField.text, !email.isEmpty,
+              let password = passwordField.text, !password.isEmpty else{
+            print("Missing Field Data") // alert
+            let alert = UIAlertController(title: "Missing Field Data", message: "Please Enter Data in The Missing Fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Try Again", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
         
+        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password) {[weak self] result, error in
+            guard let strongSelf = self else {
+                return
+            }
+            
+            guard error == nil else {
+                // show account creation failure
+                print("Account Creation Failed!")
+                
+                return
+            }
+            print("You have signed in")
+            strongSelf.emailField.isHidden = true
+            strongSelf.passwordField.isHidden = true
+        }
     }
     
     @IBAction func signInButton(_ sender: Any) {
