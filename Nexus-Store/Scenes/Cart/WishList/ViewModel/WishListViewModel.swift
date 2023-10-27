a2e654f58699352eb4f92e02c04761e6bb8ebb5f
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-27 09:22:26 -0700

    Update WishListViewModel to use CartProduct model

--- Nexus-Store/Scenes/Cart/WishList/ViewModel/WishListViewModel.swift
+++ Nexus-Store/Scenes/Cart/WishList/ViewModel/WishListViewModel.swift
@@ -28,8 +28,10 @@ class WishListViewModel {
     // MARK: - Functions
     func configCell(_ cell: ProductLandscapeCell, at index: Int) {
         let product = wishList[index]
-        let cartProduct = CartProduct(id: product.id, title: product.title ?? "Not Found", price: 0, image: product.image?.src ?? "", quantity: 0)
+        let price = product.variants?.first?.price ?? "5.0"
+        let cartProduct = CartProduct(draftOrderID: 0, variantID: 0, title: product.title ?? "Not Found", price: Double(price)!, image: product.image?.src ?? "", quantity: 0)
         cell.setProduct(cartProduct)
+        cell.setCell(id: product.id)
         cell.hideQuantity()
     }
     
