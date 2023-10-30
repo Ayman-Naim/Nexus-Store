0211a004228fed784ff0cbece15c6b11d0880d22
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-30 14:53:07 -0700

    Create LineItem Model

--- Nexus-Store/Scenes/Cart/Model/CartService.swift
+++ Nexus-Store/Scenes/Cart/Model/CartService.swift
@@ -26,36 +26,7 @@ struct DraftOrders: Codable {
 
 
 
-struct LineItem: Codable {
-    let id: Int
-    let variantID: Int?
-    let quantity: Int
-    
-    let properties: [Property]
-    
-    let productID: Int?
-    let productTitle: String
-    let productVendor: String?
-    let productPrice: String
-    
-    //    let appliedDiscount: AppliedDiscount?
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case variantID = "variant_id"
-        case quantity
-        case properties
-        case productID = "product_id"
-        case productTitle = "title"
-        case productVendor = "vendor"
-        case productPrice = "price"
-    }
-    
-    struct Property: Codable {
-        let name: String
-        let value: String
-    }
-}
+
 
 //struct ShippinAddress: Codable {
 //
