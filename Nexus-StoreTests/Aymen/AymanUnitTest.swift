cb31d75acba8c6f0a27904689f3bf803b3f784a5
Ayman-Naim <mnameno_2012@yahoo.com>
2023-11-06 21:19:06 +0200

    Testing the api calls

--- Nexus-StoreTests/Aymen/AymanUnitTest.swift
+++ Nexus-StoreTests/Aymen/AymanUnitTest.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-
+@testable import Nexus_Store
 final class AymanUnitTest: XCTestCase {
-
+    var HomeViewModel = HomeVM()
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -31,5 +31,203 @@ final class AymanUnitTest: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    //Home
+    func testgetBrands(){
+     
+        HomeViewModel.getBrands {
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let brands ):
+                XCTAssertNotNil(brands)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    func testgetPriceRole(){
+     
+        HomeViewModel.getPriceRole{
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let priceRole ):
+                XCTAssertNotNil(priceRole)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    func testgetCouponescodes(){
+        BaseUrl.priceRuleID = "-1"
+        HomeViewModel.getCouponescodes{
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let copones ):
+                XCTAssertNotNil(copones)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+
+    
+    func testgetPriceWithcoupouns(){
+      
+        HomeViewModel.getPriceWithcoupouns { bool in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch bool{
+            case true :
+                XCTAssertTrue(!self.HomeViewModel.CoupusOFPriceRule.isEmpty)
+                expectaation.fulfill()
+            case false :
+                XCTFail()
+                expectaation.fulfill()
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    
+    
+//Profile
+    var ProfileViemodel = ProfileVM()
+    func testgetOrders(){
+       
+        ProfileViemodel.getOrders{
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let orders ):
+                XCTAssertNotNil(orders)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    func testgetWishlist(){
+       
+        ProfileViemodel.getWishlist(forCustom:0){
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let orders ):
+                XCTAssertNotNil(orders)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    func testfetchAllMetafields(){
+       
+        ProfileViemodel.fetchAllMetafields(forCustom:0){
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let orders ):
+                XCTAssertNotNil(orders)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    
+
+  //Adresst Setting
+    var AddressSettingVm = AdressSettingVM()
+    
+
+    func testgetUserAdresses(){
+       
+        AddressSettingVm.getUserAdresses{
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let orders ):
+                XCTAssertNotNil(orders)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    
+    func testDelteAdress(){
+       
+        AddressSettingVm.DelteAdress(addressID:0){
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let orders ):
+                XCTAssertNotNil(orders)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    
+    //add adress viemodel
+    var AddAdressVM = AddAddressVM()
+    func testaddAdress(){
+       
+        AddAdressVM.addAdress(name: "", city: "", adddress: "", Phone: ""){
+            result in
+            let expectaation = self.expectation(description: "waiting for the api")
+            switch result{
+            case .success(let orders ):
+                XCTAssertNotNil(orders)
+                expectaation.fulfill()
+                
+            case .failure(_):
+                XCTFail()
+                expectaation.fulfill()
+                
+            }
+            self.waitForExpectations(timeout: 50000)
+        }
+    }
+    
+    
+    
+    
 
 }
